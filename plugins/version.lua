b4a08b7ff0d071283607796570fa67674a3ff6a5
focusworld <rezaabsy@yahoo.com>
2016-02-05 21:40:27 +0330

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,27 @@
+do
+
+function run(msg, matches)
+  return 'Umbrella Telegram Bot v1.5'.. [[ 
+  
+  
+  Antispam Bot: @focus_asp
+  Channel: @focus_team
+  Sudo (Admin): @mamaligod
+  Sudo (Admin): @mamaligodem
+  Sudo (Admin): @xxx_sargardan_xxx
+  
+  Powered by:
+  focus team Co. Group
+  Engineer mamali godem]]
+end
+
+return {
+  description = "Robot and Creator About", 
+  usage = "/ver : robot info",
+  patterns = {
+    "^[!/]ver$"
+  }, 
+  run = run 
+}
+
+end
